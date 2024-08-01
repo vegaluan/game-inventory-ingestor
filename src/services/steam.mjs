1d8bd73183c48876c0840230f657bc2287574fd1
vegaluan <xdlpedroxxd@icloud.com>
2024-08-01 03:02:57 -0300

    fix: bad old perf commits

--- src/services/steam.mjs
+++ src/services/steam.mjs
@@ -6,11 +6,13 @@ const getAssetName = async (assetId) => {
         
         let url = `${STEAM_API_GET_ASSET_NAME}?key=${STEAM_ID}&appid=${STEAM_APP_ID}&language=en&class_count=1&classid0=${assetId}`
         let response = await axios.get(url);
-        return response.data.result[assetId]?.name || '';
+        return {classid: response.data.result[assetId].classid, name: response.data.result[assetId].name};
     } catch (e) {
         console.error(e.message);
     }
 };
+
+
 
 const transform = async () => {
     try {
@@ -18,21 +20,29 @@ const transform = async () => {
         let res = await axios.get(url);
         let assets = res.data.result.assets || [];
 
-
-
-        const promises = assets.map(async (asset) => {
-            if (asset && asset.classid) {
-                const name =  await getAssetName(asset.classid);
-                return {
-                    classid: asset.classid,
-                    name: name,
-                    price: Object.values(asset.prices).flat().toString(),
-                };
-            }
+        const promisesGetAssetName = assets.map(asset => getAssetName(asset.classid))
+        const promisedTransformAssets = assets.map(asset => {
+            return {
+                classid: asset.classid,
+                name: '',
+                price: Object.values(asset.prices).flat().toString(),
+            };
         });
 
-        const inventory = await Promise.all(promises);
-        console.log(inventory); 
+        const resultAssets = await Promise.allSettled(promisedTransformAssets);
+        const resultNames = await Promise.allSettled(promisesGetAssetName);
+    
+        console.log(resultAssets.map((x) => x.value));
+        console.log(resultNames.map((x) => x.value));
+
+        
+/*         setTimeout((x) => {
+        console.log(workflow.map(assetItem => assetItem));
+
+        }, 10000) */
+      
+
+
     } catch (e) {
         console.error(e.message);
     }
